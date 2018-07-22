3c2f8abd167146de467f483869e9bb9afa27ba61
Joss Bleach <jossjbleach@gmail.com>
2018-07-22 12:00:37 +0100

    register state sorted out

--- client/src/components/layout/auth/Register.js
+++ client/src/components/layout/auth/Register.js
@@ -1,54 +1,93 @@
 import React, { Component } from "react";
 
 class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      password2: "",
+      errors: {}
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      confirmPassword: this.state.password2
+    };
+    console.log(newUser);
+  }
   render() {
     return (
       <div>
-        <div class="register">
-          <div class="container">
-            <div class="row">
-              <div class="col-md-8 m-auto">
-                <h1 class="display-4 text-center">Sign Up</h1>
-                <p class="lead text-center">Create your DevConnector account</p>
-                <form action="create-profile.html">
-                  <div class="form-group">
+        <div className="register">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 m-auto">
+                <h1 className="display-4 text-center">Sign Up</h1>
+                <p className="lead text-center">
+                  Create your SocialSandbox account
+                </p>
+                <form onSubmit={this.onSubmit}>
+                  <div className="form-group">
                     <input
                       type="text"
-                      class="form-control form-control-lg"
+                      className="form-control form-control-lg"
                       placeholder="Name"
                       name="name"
-                      required
+                      value={this.state.name}
+                      onChange={this.onChange}
                     />
                   </div>
-                  <div class="form-group">
+                  <div className="form-group">
                     <input
                       type="email"
-                      class="form-control form-control-lg"
+                      className="form-control form-control-lg"
                       placeholder="Email Address"
+                      value={this.state.email}
                       name="email"
+                      onChange={this.onChange}
                     />
                     <small className="form-text text-muted">
                       This site uses Gravatar so if you want a profile image,
                       use a Gravatar email
                     </small>
                   </div>
-                  <div class="form-group">
+                  <div className="form-group">
                     <input
                       type="password"
-                      class="form-control form-control-lg"
+                      className="form-control form-control-lg"
                       placeholder="Password"
+                      value={this.state.password}
                       name="password"
+                      onChange={this.onChange}
                     />
                   </div>
-                  <div class="form-group">
+                  <div className="form-group">
                     <input
                       type="password"
-                      class="form-control form-control-lg"
+                      className="form-control form-control-lg"
                       placeholder="Confirm Password"
+                      value={this.state.password2}
                       name="password2"
+                      onChange={this.onChange}
                     />
                   </div>
-                  <input type="submit" class="btn btn-info btn-block mt-4" />
+                  <input
+                    type="submit"
+                    className="btn btn-info btn-block mt-4"
+                  />
                 </form>
               </div>
             </div>
